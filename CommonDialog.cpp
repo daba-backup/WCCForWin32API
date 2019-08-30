01dc5f793d4f18290511f969ec9143e09a47f60a
Dabasan <43123127+Dabasan@users.noreply.github.com>
2019-08-30 23:15:57 +0900

    Implemented common dialogs.

---
+++ CommonDialog.cpp
@@ -0,0 +1,103 @@
+#include"CommonDialog.h"
+
+daxie::dialog::CommonDialog::CommonDialog() {
+
+}
+daxie::dialog::CommonDialog::~CommonDialog() {
+
+}
+
+daxie::dialog::ChooseColorDialog::ChooseColorDialog() {
+	memset(&cc, 0, sizeof(cc));
+
+	cc.lStructSize = sizeof(CHOOSECOLOR);
+	cc.lpCustColors = &cust_colors[0];
+	cc.Flags = CC_FULLOPEN | CC_RGBINIT;
+}
+daxie::dialog::ChooseColorDialog::~ChooseColorDialog() {
+
+}
+
+void daxie::dialog::ChooseColorDialog::SetHwndOwner(HWND hwnd) {
+	cc.hwndOwner = hwnd;
+}
+void daxie::dialog::ChooseColorDialog::SetInitialColor(COLORREF color) {
+	cc.rgbResult = color;
+}
+void daxie::dialog::ChooseColorDialog::SetFlags(DWORD flags) {
+	cc.Flags = flags;
+}
+
+BOOL daxie::dialog::ChooseColorDialog::Show() {
+	BOOL ret = ChooseColor(&cc);
+	return ret;
+}
+
+COLORREF daxie::dialog::ChooseColorDialog::GetSelectedColor() {
+	return cc.rgbResult;
+}
+
+daxie::dialog::ChooseFontDialog::ChooseFontDialog() {
+	memset(&cf, 0, sizeof(CHOOSEFONT));
+	memset(&lf, 0, sizeof(LOGFONT));
+
+	cf.lStructSize = sizeof(CHOOSEFONT);
+	cf.lpLogFont = &lf;
+	cf.Flags = CF_EFFECTS | CF_SCREENFONTS;
+}
+daxie::dialog::ChooseFontDialog::~ChooseFontDialog() {
+
+}
+
+void daxie::dialog::ChooseFontDialog::SetHwndOwner(HWND hwnd) {
+	cf.hwndOwner = hwnd;
+}
+void daxie::dialog::ChooseFontDialog::SetFlags(DWORD flags) {
+	cf.Flags = flags;
+}
+
+BOOL daxie::dialog::ChooseFontDialog::Show() {
+	BOOL ret = ChooseFont(&cf);
+	return ret;
+}
+
+LOGFONT daxie::dialog::ChooseFontDialog::GetSelectedFont() {
+	return lf;
+}
+
+daxie::dialog::FileChooser::FileChooser() {
+	memset(&ofn, 0, sizeof(OPENFILENAME));
+	
+	for (int i = 0; i < MAX_PATH; i++)filename[i] = 0;
+	for (int i = 0; i < 256; i++)custom_filter[i] = 0;
+
+	ofn.lStructSize = sizeof(OPENFILENAME);
+	ofn.lpstrFile = &filename[0];
+	ofn.nMaxFile = MAX_PATH;
+	ofn.lpstrCustomFilter = &custom_filter[0];
+	ofn.nMaxCustFilter = 256;
+	ofn.nFilterIndex = 0;
+}
+daxie::dialog::FileChooser::~FileChooser() {
+
+}
+
+void daxie::dialog::FileChooser::SetHwndOwner(HWND hwnd) {
+	ofn.hwndOwner = hwnd;
+}
+void daxie::dialog::FileChooser::SetFilter(LPCTSTR filter) {
+	ofn.lpstrFilter = filter;
+}
+void daxie::dialog::FileChooser::SetFlags(DWORD flags) {
+	ofn.Flags = flags;
+}
+
+BOOL daxie::dialog::FileChooser::ShowOpenDialog() {
+	BOOL ret = GetOpenFileName(&ofn);
+	return ret;
+}
+BOOL daxie::dialog::FileChooser::ShowSaveDialog() {
+	BOOL ret = GetSaveFileName(&ofn);
+	return ret;
+}
+
---
+++ CommonDialog.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include"Object.h"
+#include"TString.h"
+#include<Windows.h>
+#include<array>
+
+namespace daxie {
+	namespace dialog {
+		class CommonDialog :public daxie::Object {
+		public:
+			CommonDialog();
+			virtual ~CommonDialog();
+
+		public:
+			virtual void SetHwndOwner(HWND hwnd) = 0;
+			virtual void SetFlags(DWORD flags) = 0;
+
+			//virtual BOOL Show() = 0;
+		};
+
+		class ChooseColorDialog :public CommonDialog {
+		private:
+			CHOOSECOLOR cc;
+			std::array<COLORREF, 16> cust_colors;
+
+		public:
+			ChooseColorDialog();
+			virtual ~ChooseColorDialog();
+
+		public:
+			void SetHwndOwner(HWND hwnd)override;
+			void SetInitialColor(COLORREF color);
+			void SetFlags(DWORD flags)override;
+
+			BOOL Show();
+
+			COLORREF GetSelectedColor();
+		};
+
+		class ChooseFontDialog :public CommonDialog {
+		private:
+			CHOOSEFONT cf;
+			LOGFONT lf;
+
+		public:
+			ChooseFontDialog();
+			virtual ~ChooseFontDialog();
+
+		public:
+			void SetHwndOwner(HWND hwnd)override;
+			void SetFlags(DWORD flags)override;
+
+			BOOL Show();
+
+			LOGFONT GetSelectedFont();
+		};
+
+		class FileChooser :public CommonDialog {
+		private:
+			OPENFILENAME ofn;
+			std::array<TCHAR, MAX_PATH> filename;
+			std::array<TCHAR, 256> custom_filter;
+
+		public:
+			FileChooser();
+			virtual ~FileChooser();
+
+		public:
+			void SetHwndOwner(HWND hwnd)override;
+			void SetFilter(LPCTSTR filter);
+			void SetFlags(DWORD flags)override;
+
+			BOOL ShowOpenDialog();
+			BOOL ShowSaveDialog();
+		};
+	}
+}
